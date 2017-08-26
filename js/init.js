79fe5c19bf1609d247656eb186dea70890e02050
swarn1120 <swarn1120@gmail.com>
2017-08-26 12:40:51 -0500

    Comet App

--- js/init.js
+++ js/init.js
@@ -1,8 +1,14 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $(".button-collapse").sideNav();
     $('.parallax').parallax();
+    $('.modal').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
+    $('select').material_select();
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
