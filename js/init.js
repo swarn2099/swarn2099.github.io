dba0b8c5a6c06f35631d8e636546958455dc2a7d
swarn1120 <swarn1120@gmail.com>
2017-10-17 18:32:29 -0500

    Payal Dance Company

--- js/init.js
+++ js/init.js
@@ -1,18 +1,9 @@
 (function($){
   $(function(){
 
+    $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
-    $('.dropdown-button').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrain_width: false, // Does not change width of dropdown to that of the activator
-    hover: true, // Activate on hover
-    gutter: 0, // Spacing from edge
-    belowOrigin: false, // Displays dropdown below the button
-    alignment: 'left' // Displays dropdown with edge aligned to the left of button
-  }
-);
+    $('.modal').modal();
 
 
   }); // end of document ready
