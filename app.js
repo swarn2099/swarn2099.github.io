11fa853d52286f552bb4bcb4bc2ce87e298235bc
Swarn Singh <swarn2099@gmail.com>
2018-01-27 21:50:49 -0600

    Powered and Finished

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ function myFunction(){
 	localStorage.setItem("flightNumber", flightResult);
 	var final = "Hello " + str2.value + ' ' + str3.value;
 	localStorage.setItem("result", final);
+
+
+
+	var strflightStatus = '<a href="https://flightaware.com/live/flight/AA' + flightNumber.value + '" target="_blank"><figure class="image is-4by3"><img src="giphy.gif"></figure></a>'; 
+	localStorage.setItem("flightStatus", strflightStatus);
 }
 
 function loadApp() {
 	var storedValue = localStorage.getItem('result');
 	var storedValue2 = localStorage.getItem('flightNumber');
+	var storedValue3 = localStorage.getItem('flightStatus');
 	document.getElementById('demo').innerHTML = storedValue;
 	document.getElementById('demo2').innerHTML = storedValue2;
+	document.getElementById('demo3').innerHTML = storedValue3;
+
+
 }
